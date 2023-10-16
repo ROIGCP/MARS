da38909bb075ce49d2ae624f69b92ae1e6b7f215
Grant Moyle <Grant.A.Moyle@roitraining.com>
2023-10-16 08:19:28 +0530

    Update run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -3,7 +3,8 @@
 echo $GOOGLE_CLOUD_PROJECT
 
 sudo pip3 install -r requirements.txt
-gsutil cp -m gs://mars-sample/*.csv sample/
+gsutil -m cp gs://mars-sample/*.csv sample/
+# Future: this should be replaced with gcloud storage cp gs://mars-sample/*.csv sample/
 rm -R output
 python3 mars-local.py
 gsutil cp output/* gs://$GOOGLE_CLOUD_PROJECT"-bucket/local/"
