639f4b022f20b5a3d2b42c4e19a3db77c28bd070
Paul Leroy <paul@leroy.global>
2025-10-30 08:32:53 +0000

    Updated for Pluralsight PubSub Robot Account

--- prep-project.sh
+++ prep-project.sh
@@ -8,7 +8,13 @@ if [[ -z "${GOOGLE_CLOUD_PROJECT}" ]]; then
 else
     echo "Project Name: $GOOGLE_CLOUD_PROJECT"
     gcloud storage buckets create gs://$GOOGLE_CLOUD_PROJECT"-bucket" --soft-delete-duration=0
-    gcloud services enable dataflow.googleapis.com
+    gcloud services enable dataflow.googleapis.com dataflow.googleapis.com cloudfunctions.googleapis.com \
+            run.googleapis.com cloudbuild.googleapis.com eventarc.googleapis.com pubsub.googleapis.com \
+            cloudbuild.googleapis.com containerregistry.googleapis.com
+    bq mk mars
+    bq mk --schema timestamp:STRING,ipaddr:STRING,action:STRING,srcacct:STRING,destacct:STRING,amount:NUMERIC,customername:STRING -t mars.activities
+
+    echo "You will get errors below if you are running in PluralSight, ignore the errors and use the default service account instead."
     gcloud iam service-accounts create marssa
     sleep 1
     gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member serviceAccount:marssa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com --role roles/editor
@@ -16,6 +22,5 @@ else
     gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member serviceAccount:marssa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com --role roles/dataflow.worker
     sleep 1
     gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member user:$USER_EMAIL --role roles/iam.serviceAccountUser
-    bq mk mars
-    bq mk --schema timestamp:STRING,ipaddr:STRING,action:STRING,srcacct:STRING,destacct:STRING,amount:NUMERIC,customername:STRING -t mars.activities
+    
 fi
